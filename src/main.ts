cd4a9dc8e7f1fe39307e2ef2e3a1d5a630639fb2
Mike Scornavacca <52425912+scornz@users.noreply.github.com>
2023-12-02 15:49:33 -0500

    Updated basic webpage info

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,4 @@ import { MainScene } from "game/scenes/MainScene";
 new Engine({
   canvas: document.querySelector("#canvas") as HTMLCanvasElement,
   experience: MainScene,
-  info: {
-    twitter: "https://twitter.com/maya_ndljk",
-    github: "https://github.com/mayacoda/simple-threejs-typescript-starter",
-    description: "A simple Three.js + Typescript + Vite starter project",
-    documentTitle: "Three.js + Typescript + Vite",
-    title: "A cube on a plane",
-  },
 });
