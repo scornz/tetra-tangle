e8a38f88b9a1d3509f2c44b7d8e5ea17be2f73d1
Mike Scornavacca <52425912+scornz@users.noreply.github.com>
2023-12-01 22:54:33 -0500

    Added absolute path support to vite

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
-import glsl from 'vite-plugin-glsl'
-import { defineConfig } from 'vite'
+import glsl from "vite-plugin-glsl";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [glsl()],
-})
+  plugins: [glsl(), tsconfigPaths()],
+});
