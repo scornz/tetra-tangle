0cc8b0c85ef4d7c3f362cec8d2a6c2fd1d0499da
Mike Scornavacca <52425912+scornz@users.noreply.github.com>
2023-12-04 14:55:22 -0500

    Added index file to engine folder

---
+++ src/engine/index.ts
@@ -0,0 +1,16 @@
+/**
+ *  Export all engine classes and interfaces.
+ */
+
+export * from "./Camera";
+export * from "./Engine";
+export * from "./Entity";
+export * from "./Experience";
+export * from "./GameEntity";
+export * from "./Input";
+export * from "./Raycaster";
+export * from "./RenderEngine";
+export * from "./RenderLoop";
+export * from "./Resources";
+export * from "./Scene";
+export * from "./Sizes";
