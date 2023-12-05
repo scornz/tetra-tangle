6dfad75e1d97c6242ad8b9a3bd62e3234116f7c6
Mike Scornavacca <52425912+scornz@users.noreply.github.com>
2023-12-05 16:25:39 -0500

    Updated scoring data

--- src/game/data/Score.ts
+++ src/game/data/Score.ts
@@ -1,30 +1,51 @@
 /**
+ * All possible ways to score points in the game
+ */
+export enum ScoreType {
+  SINGLE,
+  DOUBLE,
+  TRIPLE,
+  TETRIS,
+  TSPIN_MINI_NONE,
+  TSPIN_NONE,
+  TSPIN_MINI_SINGLE,
+  TSPIN_SINGLE,
+  TSPIN_MINI_DOUBLE,
+  TSPIN_DOUBLE,
+  TSPIN_TRIPLE,
+  BACK_TO_BACK,
+  COMBO,
+  SOFT_DROP,
+  HARD_DROP,
+}
+
+/**
  * Score constants
  */
-export const SCORE_VALUES = {
+export const SCORE_VALUES: { [key in ScoreType]: number } = {
   /* Basic line clears */
-  SINGLE: 100,
-  DOUBLE: 300,
-  TRIPLE: 500,
-  TETRIS: 800,
+  [ScoreType.SINGLE]: 100,
+  [ScoreType.DOUBLE]: 300,
+  [ScoreType.TRIPLE]: 500,
+  [ScoreType.TETRIS]: 800,
 
   /* T-Spin line clears */
-  TSPIN_MINI_NONE: 100,
-  TSPIN_NONE: 400,
-  TSPIN_MINI_SINGLE: 200,
-  TSPIN_SINGLE: 800,
-  TSPIN_MINI_DOUBLE: 400,
-  TSPIN_DOUBLE: 1200,
-  TSPIN_TRIPLE: 1600,
+  [ScoreType.TSPIN_MINI_NONE]: 100,
+  [ScoreType.TSPIN_NONE]: 400,
+  [ScoreType.TSPIN_MINI_SINGLE]: 200,
+  [ScoreType.TSPIN_SINGLE]: 800,
+  [ScoreType.TSPIN_MINI_DOUBLE]: 400,
+  [ScoreType.TSPIN_DOUBLE]: 1200,
+  [ScoreType.TSPIN_TRIPLE]: 1600,
 
   /* Back-to-back difficult line clear multiplier */
-  BACK_TO_BACK: 1.5,
+  [ScoreType.BACK_TO_BACK]: 1.5,
 
   /* Points per consecutive combo */
-  COMBO: 50,
+  [ScoreType.COMBO]: 50,
 
   /* Points per cell moved in soft drop */
-  SOFT_DROP: 1,
+  [ScoreType.SOFT_DROP]: 1,
   /* Points per cell moved in hard drop */
-  HARD_DROP: 2,
+  [ScoreType.HARD_DROP]: 2,
 };
